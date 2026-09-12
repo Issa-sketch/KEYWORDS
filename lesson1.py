ff533d59169956fd55a4200ca32c7f637e03e639
Issa-sketch <Issabadhwal@icloud.com>
2026-09-12 17:59:54 +0100

    Add files via upload

--- lesson1.py
+++ lesson1.py
@@ -1,9 +1,41 @@
-def total_calc(bill_amount,tip_perc):
+def calculate_change(paid, price):
+    change = paid - price
+    return change
 
-    total = bill_amount*(1 + 0.01*tip_perc)
-    total = round(total, 2)
-    print(f"Please pay ${total}")
+snack_price = 75
+print("===== SNACK VENDING MACHINE =====")
+print(f"This snack costs {snack_price} units.")
+print("Accepted coins: 1, 5, 10, 25\n")
 
+total_inserted = 0
+coins_inserted = 0
 
-total_calc(150,20)
- 
+while True: 
+    coin = int(input("Insert a coin (1, 5, 10, 25 or 100:  )"))
+    if coin != 1 and coin !=5 and coin !=10 and coin !=25 and coin !=100:
+      print("Invalid coin, try again!\n")
+      continue
+
+    total_inserted += coin 
+    coins_inserted += 1 
+    print(f"Inserted {coin}. Total so far: {total_inserted}\n")
+
+    if total_inserted >= snack_price:
+        print("Enough money inserted!!!\n")
+        break
+
+change_due = calculate_change(total_inserted, snack_price)
+
+print("Dispensing your snack...")
+if change_due == 0:
+   pass
+else:
+   print(f"Here is your change: {change_due} units")
+
+print("\n===== PURCHASE SUMMARY =====")
+print("Snack Price:", snack_price)
+print("Coins Inserted:", coins_inserted)
+print("Total Paid:", total_inserted)
+print("Change Given:", change_due)
+print("=================================")
+print("Thanks for your purchase of", snack_price, "Rupees !!!")
